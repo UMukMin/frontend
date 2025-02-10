c6c277baea4b083e5d25c376af14b8af17833426
wallace <j76067469@gmail.com>
2025-02-10 17:49:13 +0000

    Implement restaurant listing and navigation; refactor Google Map integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './page';
-import GoogleMapPage from './pages/googleMap/page';
+import GoogleMapPage from './googleMap/page';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path='/google-map' element={<GoogleMapPage/>} />
+        <Route path='/googleMap' element={<GoogleMapPage/>} />
       </Routes>
     </Router>
   );
--- src/app/components/RegionButtons.tsx
+++ src/app/components/RegionButtons.tsx
@@ -1,23 +1,13 @@
 "use client"; 
 
 import React from "react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const NavigateButtons = () => {
-  const router = useRouter();
-
-  const navigateToRegion = (region: string) => {
-    router.push(`/restaurants/${region}`);
-  };
 
   return (
     <div>
-      <li onClick={() => navigateToRegion("seoul")}>서울</li>
-      <li onClick={() => navigateToRegion("busan")}>부산</li>
-      <li onClick={() => navigateToRegion("daegu")}>대구</li>
-      <li onClick={() => navigateToRegion("daejeon")}>대전</li>
-      <li onClick={() => navigateToRegion("gwangju")}>광주</li>
-      <li onClick={() => navigateToRegion("gwangju")}>제주</li>
+      <Link href="/restaurants/seoul">서울</Link>
     </div>
   );
 };
